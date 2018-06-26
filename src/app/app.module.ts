b6d737b474d82a8e81d5b342fff8fa5c84fd1850
Gioestevam <giovannigamers2014@gmail.com>
2018-06-26 13:45:42 -0300

    Routes & Firestore & Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+
+import { AppRoutingModule } from './app-routing.module';
+
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFireModule } from 'angularfire2';
+import { environment } from '../environments/environment';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    AngularFireAuthModule,
+    BrowserAnimationsModule,
+    AngularFireModule.initializeApp(environment.firebase)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyArUf6WVyofuohh8k3RHYHMrdn7l1uusYo",
+    authDomain: "bellumcerebris.firebaseapp.com",
+    databaseURL: "https://bellumcerebris.firebaseio.com",
+    projectId: "bellumcerebris",
+    storageBucket: "bellumcerebris.appspot.com",
+    messagingSenderId: "1000652140433"
+  }
 };
 
 /*
